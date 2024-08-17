15479ef549bb1638f6ca09dbf29bc21055944d83
Saad ishfaq <sishfaq984@gmail.com>
2024-08-17 15:18:59 +0500

    Added navbar component, about route, and bootstrap to the Angular app

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+    {
+        path:'about',
+        component:AboutComponent
+    }
+];
